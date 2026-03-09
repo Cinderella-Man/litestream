cdf975cb1a5f09c22a73eb66c81584d0895a0b48
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-03-09 20:17:21 +0100

    Stashing

--- lib/litestream/strategy/object_storage.ex
+++ lib/litestream/strategy/object_storage.ex
@@ -8,23 +8,56 @@ defmodule Litestream.Strategy.ObjectStorage do
   alias Litestream.Replicator
 
   @type t :: %ObjectStorage{
+          type: String.t(),
           access_key_id: String.t(),
           secret_access_key: String.t(),
-          url: String.t()
+          endpoint: String.t(),
+          bucket: String.t(),
+          path: String.t(),
+          region: String.t() | nil,
+          force_path_style: boolean() | nil,
+          skip_verify: boolean() | nil,
+          concurrency: pos_integer() | nil,
+          part_size: String.t() | nil
         }
 
-  defstruct [:access_key_id, :secret_access_key, :url]
+  defstruct [
+    :access_key_id,
+    :secret_access_key,
+    :endpoint,
+    :bucket,
+    :path,
+    :region,
+    :force_path_style,
+    :skip_verify,
+    :concurrency,
+    :part_size,
+    type: "s3"
+  ]
 
   defimpl Replicator do
     def env_vars(%ObjectStorage{access_key_id: access_key_id, secret_access_key: secret_access_key}) do
       [
-        {"AWS_ACCESS_KEY_ID", access_key_id},
-        {"AWS_SECRET_ACCESS_KEY", secret_access_key}
+        {"LITESTREAM_ACCESS_KEY_ID", access_key_id},
+        {"LITESTREAM_SECRET_ACCESS_KEY", secret_access_key}
       ]
     end
 
-    def cli_args(%ObjectStorage{url: url}, database) do
-      [database, url]
+    def cli_args(%ObjectStorage{} = config, database) do
+      args = [
+        "-type", "s3",
+        "-bucket", config.bucket,
+        "-path", config.path,
+        "-endpoint", config.endpoint
+      ]
+
+      args = if config.region, do: args ++ ["-region", config.region], else: args
+      args = if config.force_path_style, do: args ++ ["-force-path-style"], else: args
+      args = if config.skip_verify, do: args ++ ["-skip-verify"], else: args
+      args = if config.concurrency, do: args ++ ["-concurrency", to_string(config.concurrency)], else: args
+      args = if config.part_size, do: args ++ ["-part-size", config.part_size], else: args
+
+      [database | args]
     end
   end
 end
